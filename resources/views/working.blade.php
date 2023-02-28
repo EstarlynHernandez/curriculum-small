66bd2536ab1457fc2fdb1b0fbbfdf0c6f90e576f
Estarlyn <estarlin212017@hotmail.com>
2023-02-28 14:58:23 +0100

    work in progress add

---
+++ resources/views/working.blade.php
@@ -0,0 +1,19 @@
+<!DOCKTYPE HTML>
+<html lang="es">
+    <head>
+        <meta name="viewPort" content=" width=device-width. initial-scale=1.0">
+        <meta charset="UTF-8">
+        <title>Estarlyn Hernandez</title>
+    </head>
+    <body>
+        <style>
+            body{
+                background-image: url(img/r.png);
+                width: 100vw;
+                height: 100vh;
+                box-sizing: border-box;
+                overflow: hidden;
+            }
+        </style>
+    </body>
+</html>
